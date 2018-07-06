64c3f1561a57bbaa96dfd61c013cc0de080efeee
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-06 16:59:37 -0700

    MetaProgramming Practice

--- ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,9 @@
+require 'byebug'
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name.to_sym) {instance_variable_get("@#{name}")}
+      define_method("#{name}=") {|val| instance_variable_set("@#{name}", val)}
+    end
   end
 end
--- ActiveRecordLite/lib/01_sql_object.rb
+++ ActiveRecordLite/lib/01_sql_object.rb
@@ -1,22 +1,40 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
   def self.columns
     # ...
+    @col ||= DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    @col[0].map { |el| el.to_sym }
   end
 
+  # names.each do |name|
+  #   define_method(name.to_sym) {instance_variable_get("@#{name}")}
+  #   define_method("#{name}=") {|val| instance_variable_set("@#{name}", val)}
+  # end
   def self.finalize!
+    self.columns.each do |col_name|
+      define_method("#{col_name}=") {|val| self.attributes[col_name] = val}
+      define_method(col_name.to_sym) { self.attributes[col_name] }
+    end
   end
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.tableize
+    @table_name
   end
 
   def self.all
@@ -32,11 +50,17 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    # debugger
+    params.each do |k,v|
+      raise "unknown attribute '#{k}'" unless self.class.columns.include?(k.to_sym)
+
+      self.send(k.to_s+"=", v)
+    end
+
   end
 
   def attributes
-    # ...
+   @attributes ||= {}
   end
 
   def attribute_values
